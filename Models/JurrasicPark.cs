26ca79d4f57d1b02201997f2288c8445d3dcb756
JD-Fuller <jdfuller@live.com>
2020-01-24 16:17:43 -0700

    just using raptors

--- Models/JurrasicPark.cs
+++ Models/JurrasicPark.cs
@@ -9,18 +9,16 @@ namespace classes.Models
     //INSTANTIATING Lists
     public List<Raptor> Raptors { get; private set; } = new List<Raptor>();
 
-    public List<TRex> TRexs { get; private set; } = new List<TRex>();
-
-
+    // public List<TRex> TRexs { get; private set; } = new List<TRex>();
     // METHODS
     public void AddDino(Raptor raptor)
     {
       Raptors.Add(raptor);
     }
-    public void AddDino(TRex tRex)
-    {
-      TRexs.Add(tRex);
-    }
+    // public void AddDino(TRex tRex)
+    // {
+    //   TRexs.Add(tRex);
+    // }
     public void AddDino(List<Raptor> raptors)
     {
       Raptors.AddRange(raptors);
@@ -28,22 +26,18 @@ namespace classes.Models
     public void AddDino(Raptor[] raptors)
     {
       Raptors.AddRange(raptors);
-
     }
-    public void AddDino(TRex[] tRexs)
-    {
-      TRexs.AddRange(tRexs);
-    }
-    public void AddDino(List<TRex> tRexs)
-    {
-
-      TRexs.AddRange(tRexs);
-    }
-
+    // public void AddDino(TRex[] tRexs)
+    // {
+    //   TRexs.AddRange(tRexs);
+    // }
+    // public void AddDino(List<TRex> tRexs)
+    // {
+    //   TRexs.AddRange(tRexs);
+    // }
     public JurassicPark()
     {
-      // Raptors;
-
+      System.Console.WriteLine("Let's Make a Park");
 
     }
 
--- Program.cs
+++ Program.cs
@@ -9,9 +9,15 @@ namespace classes
     {
       Console.WriteLine("It's Dinosaur Time!");
       Raptor blue = new Raptor("Blue", "Purplish-blue");
-      System.Console.WriteLine(blue.Name);
+      System.Console.WriteLine(blue.Name + "&&" + blue.Color);
 
 
+
+      System.Console.WriteLine("Are you ready to make a park?");
+      JurassicPark isla = new JurassicPark();
+      System.Console.WriteLine("Congrats...you just made a park.");
+
+      isla.AddDino(blue);
 
 
 
